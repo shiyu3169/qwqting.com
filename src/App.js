4663d9d28ad492a83a6196f4f33dbd3d58335db7
Shiyu Wang <shiyu3169@gmail.com>
2019-05-22 10:01:15 -0400

    background and remove work section

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 // Style
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./css/App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './css/app.min.css';
 // Main
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Work from "./components/Work";
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Work from './components/Work';
 // Jewelry
-import Jewelry from "./components/jewelry/Jewelry";
-import Gallery from "./components/jewelry/gallery/Gallery";
-import Rendering from "./components/jewelry/Rendering";
-import LookBook from "./components/jewelry/LookBook";
-import Illusion from "./components/jewelry/gallery/Illusion";
+import Jewelry from './components/jewelry/Jewelry';
+import Gallery from './components/jewelry/gallery/Gallery';
+import Rendering from './components/jewelry/Rendering';
+import LookBook from './components/jewelry/LookBook';
+import Illusion from './components/jewelry/gallery/Illusion';
 // Illustration
-import Illustration from "./components/illustration/Illustration";
-import Footer from "./components/utility/Footer";
-import SubFooter from "./components/utility/SubFooter";
-import ScrollToTop from "./components/utility/ScrollToTop";
-import NuturingNature from "./components/jewelry/gallery/NuturingNature";
+import Illustration from './components/illustration/Illustration';
+import Footer from './components/utility/Footer';
+import SubFooter from './components/utility/SubFooter';
+import ScrollToTop from './components/utility/ScrollToTop';
+import NuturingNature from './components/jewelry/gallery/NuturingNature';
 
 function App() {
-    return (
-        <Router>
-            <ScrollToTop>
-                {/* Main */}
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/contact" component={Contact} />
-                <Route exact path="/work" component={Work} />
+  return (
+    <Router>
+      <ScrollToTop>
+        {/* Main */}
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/work' component={Work} />
 
-                {/* Jewelry */}
-                <Route exact path="/jewelry" component={Jewelry} />
-                <Route exact path="/gallery" component={Gallery} />
-                <Route exact path="/illusion" component={Illusion} />
-                <Route
-                    exact
-                    path="/nurturing_nature"
-                    component={NuturingNature}
-                />
-                <Route exact path="/lookBook" component={LookBook} />
-                <Route exact path="/rendering" component={Rendering} />
-                {/* Illustration */}
-                <Route exact path="/illustration" component={Illustration} />
-                <SubFooter />
-                <Footer />
-            </ScrollToTop>
-        </Router>
-    );
+        {/* Jewelry */}
+        <Route exact path='/jewelry' component={Jewelry} />
+        <Route exact path='/gallery' component={Gallery} />
+        <Route exact path='/illusion' component={Illusion} />
+        <Route exact path='/nurturing_nature' component={NuturingNature} />
+        <Route exact path='/lookBook' component={LookBook} />
+        <Route exact path='/rendering' component={Rendering} />
+        {/* Illustration */}
+        <Route exact path='/illustration' component={Illustration} />
+        <SubFooter />
+        <Footer />
+      </ScrollToTop>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,26 @@
-import React, { Component } from "react";
-import Navbar from "./utility/Navbar";
-import Typer from "./utility/Typer";
-import Work from "./Work";
+import React, { Component } from 'react';
+import Navbar from './utility/Navbar';
+import Typer from './utility/Typer';
+import Work from './Work';
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <header id="header-home">
-                    <div className="container">
-                        <Navbar current="home" />
-                        <div className="header-content">
-                            <h1>
-                                Qiuting Wang
-                                <br />
-                                The{" "}
-                                <Typer
-                                    words={[
-                                        "Jewelry Designer",
-                                        "Jeweler",
-                                        "Illustrator"
-                                    ]}
-                                />
-                            </h1>
-                            <div>
-                                <a href="#work" className="btn-light">
-                                    View My Work
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </header>
-                <Work />
+  render() {
+    return (
+      <div>
+        <header id='header-home'>
+          <div className='container'>
+            <Navbar current='home' />
+            <div className='header-content'>
+              <h1>
+                Qiuting Wang
+                <br />
+                The{' '}
+                <Typer words={['Jewelry Designer', 'Jeweler', 'Illustrator']} />
+              </h1>
             </div>
-        );
-    }
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
